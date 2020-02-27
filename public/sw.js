218cb5e17966e5feb14f3b4211fec859d99149c1
Roy Chen <roychen4212@gmail.com>
2020-02-28 00:24:10 +0800

    Update sw codes

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,10 @@
-if (process.browser) {
-  self.addEventListener('install', () => self.skipWaiting()); // eslint-disable-line
+self.addEventListener('install', () => self.skipWaiting()); // eslint-disable-line
 
-  self.addEventListener('activate', () => { // eslint-disable-line
-    self.registration.unregister(); // eslint-disable-line
-    self.clients.matchAll({ type: 'window' }).then(clients => { // eslint-disable-line
-      for (const client of clients) { // eslint-disable-line
-        client.navigate(client.url);
-      }
-    });
+self.addEventListener('activate', () => { // eslint-disable-line
+  self.registration.unregister(); // eslint-disable-line
+  self.clients.matchAll({ type: 'window' }).then(clients => { // eslint-disable-line
+    for (const client of clients) { // eslint-disable-line
+      client.navigate(client.url);
+    }
   });
-}
+});
